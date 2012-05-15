3eb2f9c3c8875683b0268516e3dc2c5710b53f36
Adrian Ottiker <adi@ottiker.com>
2012-05-15 10:20:05 +0000

    created dev files

---
+++ dev.js
@@ -0,0 +1,53 @@
+window.onload = function(){
+    
+    var test = {
+        
+        name: "ruedi",
+        hoi: function() {
+            
+            console.log(this.name);
+            console.log(arguments);
+            console.log(this);
+        }
+    };
+    
+    require(["./main"], function(bind) {
+        
+        bind({
+            
+            value: 12.5684,
+            element: document.getElementById("test"),
+            //selector: "#test",
+            filters: {
+                
+                fixed: 2
+            },
+            pre: "CHF ",
+            post: " Post content",
+            events: {
+                
+                "mousedown": [
+                    {
+                        object: test,
+                        method: "hoi",
+                        args: [1, 2, "3"]
+                    }
+                ],
+                
+                "mouseup": {
+                    
+                    object: test,
+                    method: "hoi",
+                    args: ["mouseup"]
+                }
+            }
+        });
+        
+        bind({
+            
+            value: "trucken",
+            element: document.getElementById("test"),
+            attr: "class"
+        });
+    });
+};
